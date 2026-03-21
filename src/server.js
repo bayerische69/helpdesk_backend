03eab69342247f24987c4a2f528e3caca914b955
JaydeMike <engraciajayde@gmail.com>
2026-03-21 22:54:17 +0800

    fixed again

--- src/server.js
+++ src/server.js
@@ -14,7 +14,7 @@ const app = express();
 
 
 app.use(cors({
-    origin: "*",
+    origin: [process.env.CORS_ORIGIN, process.env.CORS_ORIGIN_DEV],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],    
     credentials: true
