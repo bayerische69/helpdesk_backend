ebb681d6430cfe802a1e6812b668e88291d2760b
JaydeMike <engraciajayde@gmail.com>
2026-03-21 20:52:31 +0800

    ADDED SAFETY FOR CORS

--- src/server.js
+++ src/server.js
@@ -14,21 +14,13 @@ const app = express();
 const PORT = process.env.PORT || 5001;
 const __dirname = path.resolve();
 
-if(process.env.NODE_ENV !== "production") {
-  app.use(
-    cors({
-      origin: "http://localhost:3000",
-      credentials: true,
-    })
-  );
-} else {
-  app.use(
-    cors({
-      origin: "https://helpdesk-frontend-beta-liard.vercel.app",
-      credentials: true,
-    })
-  );
-}
+app.use(cors({
+  origin: [
+    process.env.CORS_ORIGIN_DEV,
+    process.env.CORS_ORIGIN
+  ],
+  credentials: true,
+}));
 
 
 app.use(cookieParser());
